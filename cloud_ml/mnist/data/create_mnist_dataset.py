a909d0785c461b1ee2276adff601cc23e8e863ed
¨Vuk <GlisovicVuk@gmail.com>
2018-12-23 16:50:20 +0100

    Finished validating dataset

--- cloud_ml/mnist/data/create_mnist_dataset.py
+++ cloud_ml/mnist/data/create_mnist_dataset.py
@@ -1,6 +1,6 @@
+from __future__ import print_function
 import os
 import logging
-import numpy as np
 import tensorflow as tf
 
 logging.basicConfig(level=logging.INFO)
@@ -8,12 +8,16 @@ logging.basicConfig(level=logging.INFO)
 
 def _int64_feature(value):
     """value should be a singleton.
+
+    Actually, the dtype coming in here should be uint8.
     """
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 
 def _bytes_feature(value):
     """value is a 2D numpy array with the image data.
+
+    Actually, the dtype coming in here should be uint8.
     """
     raw_indicator = value.tostring()
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[raw_indicator]))
@@ -46,11 +50,12 @@ def read_example_for_verification():
 
     # Read TFRecord file
     reader = tf.TFRecordReader()
-    filename_queue = tf.train.string_input_producer(['train/data.tfrecord'])
+    filename_queue = tf.train.string_input_producer(['test/data.tfrecord'])
 
     _, serialized_example = reader.read(filename_queue)
 
     # Define features
+    # Tensorflow does not allow to decode with dtype=tf.uint8, therefore use tf.int64
     read_features = {
         'image': tf.FixedLenFeature([], dtype=tf.string),
         'label': tf.FixedLenFeature([], dtype=tf.int64)
@@ -65,25 +70,32 @@ def read_example_for_verification():
     tf.train.start_queue_runners(sess)
 
     # Print features
-    image = tf.decode_raw(read_data['image'], tf.uint8).eval()
-    print(image.reshape((28, 28)))
-    label = read_data['label'].eval()
-    print(label)
+    image = tf.decode_raw(read_data['image'], tf.uint8)
+    label = read_data['label']
+    # make sure to evaluate both tensors at the same time. Otherwise one of the two will go to their next value
+    for i in xrange(3):
+        print("Visualizing number.")
+        vis_image, vis_label = sess.run([image, label])
+        # Visualize the number
+        for row in vis_image.reshape((28, 28)):
+            print(" ".join(list(map(lambda v: 'X' if v else '_', row))))
+        print(vis_label)
+        print('')
 
 
 def main():
-    # logging.info("Downloading mnist data.")
-    # (Xtrain, ytrain), (Xtest, ytest) = tf.keras.datasets.mnist.load_data()
-    #
-    # logging.info("Storing training data.")
-    # if not os.path.exists('train'):
-    #     os.mkdir('train')
-    # write_tf_records(Xtrain, ytrain, 'train/data.tfrecord')
-    #
-    # logging.info("Storing testing data.")
-    # if not os.path.exists('test'):
-    #     os.mkdir('test')
-    # write_tf_records(Xtest, ytest, 'test/data.tfrecord')
+    logging.info("Downloading mnist data.")
+    (Xtrain, ytrain), (Xtest, ytest) = tf.keras.datasets.mnist.load_data()
+
+    logging.info("Storing training data.")
+    if not os.path.exists('train'):
+        os.mkdir('train')
+    write_tf_records(Xtrain, ytrain, 'train/data.tfrecord')
+
+    logging.info("Storing testing data.")
+    if not os.path.exists('test'):
+        os.mkdir('test')
+    write_tf_records(Xtest, ytest, 'test/data.tfrecord')
 
     logging.info("Verifying stored data.")
     read_example_for_verification()
