7ed7e750364f99cb8d94ca6f9e5ddcf95d764075
VukGlisovic <>
2019-01-11 10:15:20 +0100

    Created function for creating app profiles

--- bigtable/client.py
+++ bigtable/client.py
@@ -37,8 +37,8 @@ class Bigtable(object):
             None
         """
         if self.instance.exists():
-            raise AlreadyExists("Instance %s already exists.", self.instance_id)
-        logging.info("Creating instance %s", self.instance_id)
+            raise AlreadyExists("Instance '{}' already exists.".format(self.instance_id))
+        logging.info("Creating instance '%s'.", self.instance_id)
         # instance configurations
         production, nr_nodes = (bt_enums.Instance.Type.PRODUCTION, nr_nodes) if create_in_production else (bt_enums.Instance.Type.DEVELOPMENT, None)
         self.instance = self.client.instance(instance_id=self.instance_id, display_name=self.instance_id, instance_type=production)
@@ -54,6 +54,40 @@ class Bigtable(object):
         # We want to make sure the operation completes.
         operation.result(timeout=timeout)
 
+    def create_application_profile(self, app_profile_id, description, cluster_id, multi_cluster_routing, allow_transactional_writes):
+        """Adds an application profile to a cluster of an instance. There's two main
+        parameters to configure for an application profile:
+        1) single or multi cluster routing:
+            with multi-cluster routing, if for some reason a transaction fails for one
+            cluster, it will automatically be rerouted to another cluster. An advantage
+            for single cluster routing is that it helps isolate CPU-intensive loads.
+        2) single row transactions:
+            if enabled, then read-modify-write and check-and-mutate operations are
+            allowed.
+        You can find more info here: https://cloud.google.com/bigtable/docs/app-profiles
+
+        Args:
+            app_profile_id (str): name of the app profile
+            description (str):
+            cluster_id (str): name of the cluster to add the app profile to
+            multi_cluster_routing (bool): whether to route data to a single cluster or
+                to multiple clusters.
+            allow_transactional_writes (bool): can only be true for single cluster routing.
+
+        Returns:
+            None
+        """
+        routing_policy_type = bt_enums.RoutingPolicyType.ANY if multi_cluster_routing else bt_enums.RoutingPolicyType.SINGLE
+
+        app_profile = self.instance.app_profile(
+            app_profile_id=app_profile_id,
+            routing_policy_type=routing_policy_type,
+            description=description,
+            cluster_id=cluster_id,
+            allow_transactional_writes=allow_transactional_writes
+        )
+        app_profile.create(ignore_warnings=True)
+
     def delete_instance(self):
         """Delete the instance. At the same time resets the instance attribute such
         that a new instance with the same instance_id can be created.
@@ -61,7 +95,7 @@ class Bigtable(object):
         Returns:
             None
         """
-        logging.warning("Deleting instance %s", self.instance_id)
+        logging.warning("Deleting instance '%s'", self.instance_id)
         self.instance.delete()
         # Reset the instance attribute
         self.instance = self.client.instance(self.instance_id)
