85fed223da83508cd6344d37b12708a42f799491
VukGlisovic <>
2019-01-22 16:11:47 +0100

    Updated requirements file

--- app_engine/calculator/build_virtualenv.sh
+++ app_engine/calculator/build_virtualenv.sh
@@ -2,6 +2,7 @@
 
 SOURCE_DIR=""$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )""
 VE_DIR="${SOURCE_DIR}/ve"
+REQUIREMENTS_FILE="${SOURCE_DIR}/requirements.txt"
 
 echo "Removing old environment."
 rm -rf ${VE_DIR}
@@ -11,4 +12,4 @@ virtualenv -p python3 ${VE_DIR}
 
 source ${VE_DIR}/bin/activate
 
-pip install numpy
+pip install -r ${REQUIREMENTS_FILE}
