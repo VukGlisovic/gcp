8469500cb49c6447dca8af322942b446d4ee8508
¨Vuk <GlisovicVuk@gmail.com>
2019-01-26 17:16:22 +0100

    Refactored input parameters for mnist training task

--- cloud_ml/examples/mnist/trainer/task.py
+++ cloud_ml/examples/mnist/trainer/task.py
@@ -8,32 +8,40 @@ logging.basicConfig(level=logging.INFO)
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--features_file',
-                    help='Filepath where features for training are contained (these should correspond to the labels file).',
+parser.add_argument('--train_data_folder',
+                    help='Path to folder containing the training data. This folder should have '
+                         'two files: features.tfrecord and labels.tfrecord',
                     type=str,
                     default='../data/train/features.tfrecord')
-parser.add_argument('--labels_file',
-                    help='Filepath where labels for training are contained (these should correspond to the features file).',
+parser.add_argument('--evaluation_data_folder',
+                    help='Path to folder containing the evaluation data. This folder should likewise'
+                         'containt two files.',
                     type=str,
-                    default='../data/train/labels.tfrecord')
+                    default='')
 parser.add_argument('--model_dir',
                     help='Output directory for storing checkpoints and model.',
                     type=str,
                     default=os.path.join(os.environ['HOME'], 'gcp/cloud_ml/examples/mnist/outputs/'))
+parser.add_argument('--nr_epochs',
+                    help='Number of times to iterate over the entire dataset.',
+                    type=float,
+                    default=1)
 parser.add_argument('--learning_rate',
                     help='Learning rate for the optimizer.',
                     type=float,
                     default=0.0001)
 
 known_args, _ = parser.parse_known_args()
-features_file = known_args.features_file
-labels_file = known_args.labels_file
+train_data_folder = known_args.train_data_folder
+evaluation_data_folder = known_args.evaluation_data_folder
 model_dir = known_args.model_dir
+nr_epochs = known_args.nr_epochs
 learning_rate = known_args.learning_rate
 logging.info("Input parameters:")
-logging.info("Features filepath: %s", features_file)
-logging.info("Labels filepath: %s", labels_file)
+logging.info("Train data folder filepath: %s", train_data_folder)
+logging.info("Evaluation data folder filepath: %s", evaluation_data_folder)
 logging.info("Output directory: %s", model_dir)
+logging.info("Number of epochs: %s", nr_epochs)
 logging.info("Learning rate: %s", learning_rate)
 
 
@@ -49,10 +57,16 @@ def run():
 
     logging.info("Starting training of mnist model.")
     logging.info("You can checkout tensorboard with the following command:\ntensorboard --logdir='%s'", model_dir)
-    classifier.train(input_fn=lambda: input_fn(features_file, labels_file, epochs=1, batch_size=32, buffer_size=50))
+    train_features_file = os.path.join(train_data_folder, 'features.tfrecord')
+    train_labels_file = os.path.join(train_data_folder, 'labels.tfrecord')
+    classifier.train(input_fn=lambda: input_fn(train_features_file, train_labels_file, epochs=nr_epochs, batch_size=32, buffer_size=50))
 
-    logging.info("Evaluate accuracy of the model.")
-    classifier.evaluate(input_fn=lambda: input_fn('../data/test/features.tfrecord', '../data/test/labels.tfrecord', epochs=1, batch_size=50, buffer_size=0))
+    if evaluation_data_folder:
+        logging.info("Evaluate accuracy of the model.")
+        evaluation_features_file = os.path.join(evaluation_data_folder, 'features.tfrecord')
+        evaluation_labels_file = os.path.join(evaluation_data_folder, 'labels.tfrecord')
+        result = classifier.evaluate(input_fn=lambda: input_fn(evaluation_features_file, evaluation_labels_file, epochs=1, batch_size=50, buffer_size=0))
+        logging.info("Accuracy on evaluation set: %.3f", result['accuracy'])
 
 
 if __name__ == '__main__':
