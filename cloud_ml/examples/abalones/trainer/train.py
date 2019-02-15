390622cfe898c4e71aa35b24b85feb6260726025
VukGlisovic <>
2019-02-15 10:25:19 +0100

    Training an xgb model

--- cloud_ml/examples/abalones/trainer/train.py
+++ cloud_ml/examples/abalones/trainer/train.py
@@ -12,23 +12,38 @@ parser.add_argument('--data_path',
                     help='Path to file containing the training data.',
                     type=str,
                     default=os.path.join(os.environ['HOME'], 'gcp/cloud_ml/examples/abalones/data/abalone_data.csv'))
+parser.add_argument('--model_path',
+                    help='Path where to store the model.',
+                    type=str,
+                    default=os.path.join(os.environ['HOME'], 'gcp/cloud_ml/examples/abalones/outputs/xgb_abalones.bst'))
 
 known_args, _ = parser.parse_known_args()
 data_path = known_args.data_path
+model_path = known_args.model_path
 logging.info("Input parameters:")
 logging.info("Train data filepath: %s", data_path)
+logging.info("Outputs filepath: %s", model_path)
 
 
-FEATURE_COLUMNS = ['Sex', 'Length', 'Diameter', 'Height', 'Whole weight', 'Shucked weight', 'Viscera weight', 'Shell weight']
 TARGET_COLUMN = 'Rings'
-COLUMNS = FEATURE_COLUMNS + [TARGET_COLUMN]
+COLUMNS = ['Sex', 'Length', 'Diameter', 'Height', 'Whole weight', 'Shucked weight', 'Viscera weight', 'Shell weight'] + [TARGET_COLUMN]
 
 
 # reading data from local or cloud storage path
 with open(data_path, 'r') as train_data:
     data = pd.read_csv(train_data, header=None, names=COLUMNS)
+data = pd.get_dummies(data, columns=['Sex'], drop_first=True)
 
-Xtrain = data[FEATURE_COLUMNS]
+Xtrain = data[data.columns.drop(TARGET_COLUMN)]
 ytrain = data[TARGET_COLUMN]
 
+dtrain = xgb.DMatrix(Xtrain, ytrain)
 
+logging.info("Starting training...")
+
+params = {'objective': 'reg:linear'}
+bst = xgb.train(params, dtrain, num_boost_round=20)
+
+logging.info("Finished training!")
+
+bst.save_model(model_path)
