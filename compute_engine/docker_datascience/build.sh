4214efe4fb65107c154e54d9132cdd2407aa0166
¨Vuk <GlisovicVuk@gmail.com>
2019-03-17 08:37:42 +0100

    removed data folder from dockerfile

--- compute_engine/docker_datascience/build.sh
+++ compute_engine/docker_datascience/build.sh
@@ -4,7 +4,7 @@
 # docker run --rm -it <id_last_working_layer> bash -il
 
 # start new (detached) container with port forwarding (--rm removes the container after shutdown of the container):
-# docker run --rm -d -p 8889:8889 <image_id>
+# docker run --rm --name datascience_container -d -v /home/<path_to_your_folder>:/home/vukglisovic/python_scripts -p 8889:8889 <image_id>
 
 # enter a running container as root user:
 # sudo docker exec -it -u root <container_id> bash
